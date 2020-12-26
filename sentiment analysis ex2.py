999c67a95ff197f513a04603bc3156131221b058
Ritesh gupta <55602425+riteshgcoder@users.noreply.github.com>
2020-12-27 00:43:32 +0530

    Add files via upload

---
+++ sentiment analysis ex2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from textblob import TextBlob
+
+
+# In[12]:
+
+
+y=input('Type your message:')
+ritesh=TextBlob(y)
+x=ritesh.sentiment.polarity
+# -ve=x<0 and neutral=0 and +ve x>0 && x<=1
+if x<0:
+    print('Negative')
+elif x==0:
+    print('Neutral')
+elif x>0 and x<=1:
+    print('Positive')
+
+
+# In[ ]:
+
+
+
+
